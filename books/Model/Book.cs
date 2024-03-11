d44e9aa75ebf6b8d204de64e354cb14c8f97ecaa
JP Gouigoux <jp.gouigoux@free.fr>
2024-03-11 22:34:26 +0100

    Removing minimal API project

--- books/Model/Book.cs
+++
@@ -1,9 +0,0 @@
-public class Book
-{
-    public string BusinessId { get; set; }
-    public string? ISBN { get; set; }
-    public int? NumberOfPages { get; set; }
-    public DateOnly? PublishDate { get; set; }
-    public EditingPetal? Editing { get; set; }
-    public SalesPetal Sales { get; set; }
-}
--- books/Model/SalesPetal.cs
+++
@@ -1,5 +0,0 @@
-public class SalesPetal
-{
-    public decimal? Price { get; set; }
-    public decimal? Weight { get; set; }
-}
