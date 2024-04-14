5e2a0afd39b18562e6bf07ce89bafe08988ce5b0
JP Gouigoux <jp.gouigoux@free.fr>
2024-04-14 14:45:15 +0200

    Author auto-patch form

--- portal-gui/DTO/Author.cs
+++ portal-gui/DTO/Author.cs
@@ -8,6 +8,65 @@ public class Author
     public string? UserEmailAddress { get; set; }
     public string? Restriction { get; set; }
     public ContactsPetal? Contacts { get; set; }
+
+    public Address MainAddress
+    {
+        get
+        {
+            if (Contacts == null) Contacts = new ContactsPetal();
+            if (Contacts.Addresses == null) Contacts.Addresses = new List<Address>();
+            if (Contacts.Addresses.Count() == 0) Contacts.Addresses.Add(new Address() { Country = new() });
+            return Contacts.Addresses.First();
+        }
+    }
+
+    public string? HomePhone
+    {
+        get
+        {
+            Phone home = Contacts?.Phones?.FirstOrDefault(p => p.IANAType == "home");
+            if (home != null) return home.Number;
+            return "N/A";
+        }
+        set
+        {
+            Phone home = Contacts?.Phones?.FirstOrDefault(p => p.IANAType == "home");
+            if (home != null) home.Number = value;
+            else
+            {
+                if (Contacts == null) Contacts = new ContactsPetal();
+                if (Contacts.Phones == null) Contacts.Phones = new List<Phone>();
+                Contacts.Phones.Add(new Phone() { IANAType = "home", Number = value });
+            }
+        }
+    }
+
+    public string? MobilePhone
+    {
+        get
+        {
+            Phone cell = Contacts?.Phones?.FirstOrDefault(p => p.IANAType == "cell");
+            if (cell != null) return cell.Number;
+            return "N/A";
+        }
+        set
+        {
+            Phone cell = Contacts?.Phones?.FirstOrDefault(p => p.IANAType == "cell");
+            if (cell != null) cell.Number = value;
+            else
+            {
+                if (Contacts == null) Contacts = new ContactsPetal();
+                if (Contacts.Phones == null) Contacts.Phones = new List<Phone>();
+                Contacts.Phones.Add(new Phone() { IANAType = "cell", Number = value });
+            }
+        }
+    }
+
+    public bool IsBlocked
+    {
+        get { return Restriction != "none"; }
+        set { if (value) Restriction = "blocked"; else Restriction = "none"; }
+    }
 }
 
 public class ContactsPetal
