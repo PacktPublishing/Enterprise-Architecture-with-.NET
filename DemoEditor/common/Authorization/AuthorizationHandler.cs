e216d329bd15b5b7c9ff9c8d706b5d7cc1a900b8
JP Gouigoux <jp.gouigoux@free.fr>
2024-05-20 22:22:32 +0200

    Moving authorization classes in common project and adjusting Docker Compose build structure

--- DemoEditor/common/Authorization/AuthorizationHandler.cs
+++ DemoEditor/common/Authorization/AuthorizationHandler.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Authorization;
-
-namespace DemoEditor.Authorization;
+using Microsoft.AspNetCore.Http;
 
 public class EditorRequirement : IAuthorizationRequirement
 {
@@ -20,6 +19,42 @@ public class EditorAuthorizationHandler : AuthorizationHandler<EditorRequirement
             if (role.Value == "editor") context.Succeed(requirement);
             if (role.Value == "director") context.Succeed(requirement);
         }
+        return Task.CompletedTask;
+    }
+}
+
+public class EditorApiKeyRequirement : IAuthorizationRequirement
+{
+    public EditorApiKeyRequirement()
+    {
+    }
+}
+
+public class EditorApiKeyAuthorizationHandler : AuthorizationHandler<EditorApiKeyRequirement>
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    private readonly IApiKeyValidation _apiKeyValidation;
+
+    public EditorApiKeyAuthorizationHandler(IHttpContextAccessor httpContextAccessor, IApiKeyValidation apiKeyValidation)
+    {
+        _httpContextAccessor = httpContextAccessor;
+        _apiKeyValidation = apiKeyValidation;
+    }
+
+    protected override Task HandleRequirementAsync(
+        AuthorizationHandlerContext context, 
+        EditorApiKeyRequirement requirement)
+    {
+        foreach (var role in context.User.Claims.Where(claim => claim.Type == "realm_roles"))
+        {
+            if (role.Value == "editor") context.Succeed(requirement);
+            if (role.Value == "director") context.Succeed(requirement);
+        }
+
+        string apiKey = _httpContextAccessor?.HttpContext?.Request.Headers["X-API-Key"].ToString();
+        if (!string.IsNullOrWhiteSpace(apiKey) && _apiKeyValidation.IsValidApiKey(apiKey)) context.Succeed(requirement);
+
         return Task.CompletedTask;
     }
 }
