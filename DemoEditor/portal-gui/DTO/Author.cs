9dbfc33bddb7821199d052d01e500845e50161c7
JP Gouigoux <jp.gouigoux@free.fr>
2024-06-01 11:10:32 +0200

    Using shorter syntax for initialization taking into account potential initial not null value

--- DemoEditor/portal-gui/DTO/Author.cs
+++ DemoEditor/portal-gui/DTO/Author.cs
@@ -13,10 +13,10 @@ public class Author
     {
         get
         {
-            if (Contacts == null) Contacts = new ContactsPetal();
-            if (Contacts.Addresses == null) Contacts.Addresses = new List<Address>();
+            Contacts ??= new ContactsPetal();
+            Contacts.Addresses ??= new List<Address>();
             if (Contacts.Addresses.Count() == 0) Contacts.Addresses.Add(new Address() { Country = new() });
-            if (Contacts.Addresses[0].Country == null) Contacts.Addresses[0].Country = new();
+            Contacts.Addresses[0].Country ??= new();
             return Contacts.Addresses.First();
         }
     }
